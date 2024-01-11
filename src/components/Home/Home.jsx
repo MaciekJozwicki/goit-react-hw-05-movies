6fa18f3fd7dacc35ee720ab87adff55415f4d2bd
Maciek <maciekjozwicki@wp.pl>
2024-01-11 01:37:38 +0100

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,9 +3,9 @@ import Loader from 'components/Loader/Loader';
 import MovieResults from 'components/MovieResults/MovieResults';
 
 export const Home = () => {
-  const { data, loading } = useGetMovies(
+  const { data } = useGetMovies(
     'https://api.themoviedb.org/3/trending/all/day'
   );
- 
+
   return <>{!data ? <Loader /> : <MovieResults data={data} />}</>;
 };
