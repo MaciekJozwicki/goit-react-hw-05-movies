e7b15dcb53f16f4e0104be67bde52b05e5a614cd
Maciek <maciekjozwicki@wp.pl>
2024-01-14 00:56:10 +0100

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,21 +1,8 @@
-import SearchPage from 'pages/SearchPage/SearchPage';
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router';
 import { Home } from '../../components/Home/Home';
 import Button from 'components/Button/Button';
 import { Link } from 'react-router-dom';
 
 const HomePage = () => {
-  // const location = useLocation();
-
-  // const searchBar = location.state?.searchBar;
-
-  // useEffect(() => {
-  //   if (searchBar && searchBar === true) {
-  //     setPage(true);
-  //   }
-  // }, [searchBar]);
-
   return (
     <>
       <Link to="/search">
